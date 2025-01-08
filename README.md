6ee7d20b74dbae7b8580c97ecda8a23860a47493
琦琦 <sgq457@qq.com>
2025-01-08 20:24:39 +0800

    备份

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Qii Theme
 
 ## 欢迎使用
-一个 VSCode 主题，基于 One Monokai Theme。
+一个 VSCode 主题，基于 One Monokai Theme 主题修改而来。
 
--- demo/javaScript.js
+++ demo/javaScript.js
@@ -1,29 +1,29 @@
 import _, { every, foo } from 'lodash';
 
 var allTrue = every([
-  10 === 3123213,
-  1 === '1',
-  false
+    10 === 3123213,
+    1 === '1',
+    false
 ])
 
 let someTrue = _.some([
-  true,
-  typeof 2 == 'string'
+    true,
+    typeof 2 == 'string'
 ])
 
 // Class.
 class Dog extends Animal {
-  bark() {
-    console.log('Woof!')
-    this.bark
-  }
+    bark() {
+        console.log('Woof!')
+        this.bark
+    }
 }
 
 export default Dog;
 
 export function greet(name) {
-  let message = `Hello ${name}`;
-	console.log(message);
+    let message = `Hello ${name}`;
+    console.log(message);
 }
 
 greet('Foo');
@@ -36,42 +36,42 @@ const https = require('https');
  * @param String URL
  */
 const makeRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      res.setEncoding('utf8');
-      let body = '';
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
 
-      res.on('data', data => {
-        body += data;
-      });
+            res.on('data', data => {
+                body += data;
+            });
 
-      res.on('end', () => {
-        resolve(JSON.parse(body));
-      });
+            res.on('end', () => {
+                resolve(JSON.parse(body));
+            });
 
-    }).on('error', reject);
-  });
+        }).on('error', reject);
+    });
 }
 
 async function getUser(id) {
-  let user = await makeRequest(`https://api.github.com/users/${id}`);
-  return user
+    let user = await makeRequest(`https://api.github.com/users/${id}`);
+    return user
 }
 
 getUser(4);
 
 // If/else.
 if (condition) {
-	//
+    //
 } else if (condition2) {
-	//
+    //
 } else {
-	//
+    //
 }
 
 // For loop
-for(let i = 0; i < 5; i++) {
-  // loop
+for (let i = 0; i < 5; i++) {
+    // loop
 }
 
 const validFormat = /\d[a-z]\.*\d{4}/i;
